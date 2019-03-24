e5d12613956f84c3f6cb5864f4ae575c0a0e5e93
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2019-03-25 00:40:20 +0530

    shopping cart

---
+++ trackit/app/src/main/java/com/example/trackit/BPActivity.java
@@ -0,0 +1,13 @@
+package com.example.trackit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bp);
+    }
+}
